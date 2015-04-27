kyoren/https-github.com-h2oai-h2o-3

PUBDEV-711: fix the Python binding and PyUnit tests.

h2o-py/h2o/h2o.py
@@ -30,20 +30,20 @@ def _import1(path):
   j = H2OConnection.get_json(url_suffix="ImportFiles", path=path)
   if j['fails']:
     raise ValueError("ImportFiles of " + path + " failed on " + j['fails'])
-  return j['keys'][0]
+  return j['frame_ids'][0]
 
-def upload_file(path, destination_key=""):
+def upload_file(path, destination_frame=""):
   """
   Upload a dataset at the path given from the local machine to the H2O cluster.
 
   :param path: A path specifying the location of the data to upload.
-  :param destination_key: The name of the H2O Frame in the H2O Cluster.
+  :param destination_frame: The name of the H2O Frame in the H2O Cluster.
   :return: A new H2OFrame
   """
   fui = {"file": os.path.abspath(path)}
-  dest_key = H2OFrame.py_tmp_key() if destination_key == "" else destination_key
-  H2OConnection.post_json(url_suffix="PostFile", file_upload_info=fui,destination_key=dest_key)
-  return H2OFrame(text_key=dest_key)
+  destination_frame = H2OFrame.py_tmp_key() if destination_frame == "" else destination_frame
+  H2OConnection.post_json(url_suffix="PostFile", file_upload_info=fui,destination_frame=destination_frame)
+  return H2OFrame(text_key=destination_frame)
 
 
 def import_frame(path=None, vecs=None):
@@ -56,15 +56,15 @@ def import_frame(path=None, vecs=None):
   return H2OFrame(vecs=vecs) if vecs else H2OFrame(remote_fname=path)
 
 
-def parse_setup(rawkey):
+def parse_setup(raw_frames):
   """
-  :param rawkey: A collection of imported file keys
+  :param raw_frames: A collection of imported file frames
   :return: A ParseSetup "object"
   """
 
   # So the st00pid H2O backend only accepts things that are quoted (nasty Java)
-  if isinstance(rawkey, unicode): rawkey = [rawkey]
-  j = H2OConnection.post_json(url_suffix="ParseSetup", source_keys=[_quoted(key) for key in rawkey])
+  if isinstance(raw_frames, unicode): raw_frames = [raw_frames]
+  j = H2OConnection.post_json(url_suffix="ParseSetup", source_frames=[_quoted(id) for id in raw_frames])
   if not j['is_valid']:
     raise ValueError("ParseSetup not Valid", j)
   return j
@@ -72,7 +72,7 @@ def parse_setup(rawkey):
 
 def parse(setup, h2o_name, first_line_is_header=(-1, 0, 1)):
   """
-  Trigger a parse; blocking; removeFrame just keep the Vec keys.
+  Trigger a parse; blocking; removeFrame just keep the Vecs.
 
   :param setup: The result of calling parse_setup.
   :param h2o_name: The name of the H2O Frame on the back end.
@@ -80,7 +80,7 @@ def parse(setup, h2o_name, first_line_is_header=(-1, 0, 1)):
   :return: A new parsed object  
   """
   # Parse parameters (None values provided by setup)
-  p = { 'destination_key' : h2o_name,
+  p = { 'destination_frame' : h2o_name,
         'parse_type' : None,
         'separator' : None,
         'single_quotes' : None,
@@ -113,7 +113,7 @@ def parse(setup, h2o_name, first_line_is_header=(-1, 0, 1)):
   p["check_header"] = first_line_is_header
 
   # Extract only 'name' from each src in the array of srcs
-  p['source_keys'] = [_quoted(src['name']) for src in setup['source_keys']]
+  p['source_frames'] = [_quoted(src['name']) for src in setup['source_frames']]
 
   # Request blocking parse
   j = H2OJob(H2OConnection.post_json(url_suffix="Parse", **p), "Parse").poll()
@@ -243,17 +243,17 @@ def ipy_get_leading_spaces(line):
     if c in [' ', '\t']: spaces += c
     else: return spaces
 
-def remove(key):
+def remove(id):
   """
-  Remove key from H2O.
+  Remove object from H2O.
 
-  :param key: The key pointing to the object to be removed.
+  :param id: The id pointing to the object to be removed.
   :return: Void
   """
-  if key is None:
-    raise ValueError("remove with no key is not supported, for your protection")
+  if id is None:
+    raise ValueError("remove with no id is not supported, for your protection")
 
-  H2OConnection.delete("DKV/" + key)
+  H2OConnection.delete("DKV/" + id)
 
 def rapids(expr):
   """
@@ -267,14 +267,23 @@ def rapids(expr):
     raise EnvironmentError("rapids expression not evaluated: {0}".format(str(result['error'])))
   return result
 
-def frame(key):
+def frame(frame_id):
   """
-  Retrieve metadata for a key that points to a Frame.
+  Retrieve metadata for a id that points to a Frame.
 
-  :param key: A pointer to a Frame  in H2O.
+  :param frame_id: A pointer to a Frame  in H2O.
   :return: Meta information on the frame
   """
-  return H2OConnection.get_json("Frames/" + key)
+  return H2OConnection.get_json("Frames/" + frame_id)
+
+
+def frames():
+  """
+  Retrieve all the Frames.
+
+  :return: Meta information on the frames
+  """
+  return H2OConnection.get_json("Frames")
 
 def frame_summary(key):
   """
@@ -317,10 +326,10 @@ def cbind(left,right):
   j = frame(fr)
   fr = j['frames'][0]
   rows = fr['rows']
-  veckeys = fr['vec_keys']
+  vec_ids = fr['vec_ids']
   cols = fr['columns']
   colnames = [col['label'] for col in cols]
-  result = H2OFrame(vecs=H2OVec.new_vecs(zip(colnames, veckeys), rows))
+  result = H2OFrame(vecs=H2OVec.new_vecs(zip(colnames, vec_ids), rows))
   result.setNames(names)
   return result
 

venkatesh12341234/h2o-dev

h2o-py/h2o/h2o.py
@@ -30,20 +30,20 @@ def _import1(path):
   j = H2OConnection.get_json(url_suffix="ImportFiles", path=path)
   if j['fails']:
     raise ValueError("ImportFiles of " + path + " failed on " + j['fails'])
-  return j['keys'][0]
+  return j['frame_ids'][0]
 
-def upload_file(path, destination_key=""):
+def upload_file(path, destination_frame=""):
   """
   Upload a dataset at the path given from the local machine to the H2O cluster.
 
   :param path: A path specifying the location of the data to upload.
-  :param destination_key: The name of the H2O Frame in the H2O Cluster.
+  :param destination_frame: The name of the H2O Frame in the H2O Cluster.
   :return: A new H2OFrame
   """
   fui = {"file": os.path.abspath(path)}
-  dest_key = H2OFrame.py_tmp_key() if destination_key == "" else destination_key
-  H2OConnection.post_json(url_suffix="PostFile", file_upload_info=fui,destination_key=dest_key)
-  return H2OFrame(text_key=dest_key)
+  destination_frame = H2OFrame.py_tmp_key() if destination_frame == "" else destination_frame
+  H2OConnection.post_json(url_suffix="PostFile", file_upload_info=fui,destination_frame=destination_frame)
+  return H2OFrame(text_key=destination_frame)
 
 
 def import_frame(path=None, vecs=None):
@@ -56,15 +56,15 @@ def import_frame(path=None, vecs=None):
   return H2OFrame(vecs=vecs) if vecs else H2OFrame(remote_fname=path)
 
 
-def parse_setup(rawkey):
+def parse_setup(raw_frames):
   """
-  :param rawkey: A collection of imported file keys
+  :param raw_frames: A collection of imported file frames
   :return: A ParseSetup "object"
   """
 
   # So the st00pid H2O backend only accepts things that are quoted (nasty Java)
-  if isinstance(rawkey, unicode): rawkey = [rawkey]
-  j = H2OConnection.post_json(url_suffix="ParseSetup", source_keys=[_quoted(key) for key in rawkey])
+  if isinstance(raw_frames, unicode): raw_frames = [raw_frames]
+  j = H2OConnection.post_json(url_suffix="ParseSetup", source_frames=[_quoted(id) for id in raw_frames])
   if not j['is_valid']:
     raise ValueError("ParseSetup not Valid", j)
   return j
@@ -72,7 +72,7 @@ def parse_setup(rawkey):
 
 def parse(setup, h2o_name, first_line_is_header=(-1, 0, 1)):
   """
-  Trigger a parse; blocking; removeFrame just keep the Vec keys.
+  Trigger a parse; blocking; removeFrame just keep the Vecs.
 
   :param setup: The result of calling parse_setup.
   :param h2o_name: The name of the H2O Frame on the back end.
@@ -80,7 +80,7 @@ def parse(setup, h2o_name, first_line_is_header=(-1, 0, 1)):
   :return: A new parsed object  
   """
   # Parse parameters (None values provided by setup)
-  p = { 'destination_key' : h2o_name,
+  p = { 'destination_frame' : h2o_name,
         'parse_type' : None,
         'separator' : None,
         'single_quotes' : None,
@@ -113,7 +113,7 @@ def parse(setup, h2o_name, first_line_is_header=(-1, 0, 1)):
   p["check_header"] = first_line_is_header
 
   # Extract only 'name' from each src in the array of srcs
-  p['source_keys'] = [_quoted(src['name']) for src in setup['source_keys']]
+  p['source_frames'] = [_quoted(src['name']) for src in setup['source_frames']]
 
   # Request blocking parse
   j = H2OJob(H2OConnection.post_json(url_suffix="Parse", **p), "Parse").poll()
@@ -232,29 +232,29 @@ def ipy_lines(block):
   else:
     raise NotImplementedError, "ipython notebook source/line json format not handled"
 
-def remove(key):
+def remove(object):
   """
-  Remove key from H2O.
+  Remove object from H2O.
 
-  :param key: The key pointing to the object to be removed.
+  :param object: The object pointing to the object to be removed.
   :return: Void
   """
-  if key is None:
-    raise ValueError("remove with no key is not supported, for your protection")
+  if object is None:
+    raise ValueError("remove with no object is not supported, for your protection")
 
-  if isinstance(key, H2OFrame):
-    key._vecs=[]
+  if isinstance(object, H2OFrame):
+    object._vecs=[]
 
-  elif isinstance(key, H2OVec):
-    H2OConnection.delete("DKV/"+str(key.key()))
-    key._expr=None
-    key=None
+  elif isinstance(object, H2OVec):
+    H2OConnection.delete("DKV/"+str(object.key()))
+    object._expr=None
+    object=None
 
   else:
-    H2OConnection.delete("DKV/" + key)
+    H2OConnection.delete("DKV/" + object)
   #
   # else:
-  #   raise ValueError("Can't remove objects of type: " + key.__class__)
+  #   raise ValueError("Can't remove objects of type: " + id.__class__)
 
 def rapids(expr):
   """
@@ -268,14 +268,23 @@ def rapids(expr):
     raise EnvironmentError("rapids expression not evaluated: {0}".format(str(result['error'])))
   return result
 
-def frame(key):
+def frame(frame_id):
   """
-  Retrieve metadata for a key that points to a Frame.
+  Retrieve metadata for a id that points to a Frame.
 
-  :param key: A pointer to a Frame  in H2O.
+  :param frame_id: A pointer to a Frame  in H2O.
   :return: Meta information on the frame
   """
-  return H2OConnection.get_json("Frames/" + key)
+  return H2OConnection.get_json("Frames/" + frame_id)
+
+
+def frames():
+  """
+  Retrieve all the Frames.
+
+  :return: Meta information on the frames
+  """
+  return H2OConnection.get_json("Frames")
 
 def frame_summary(key):
   """
@@ -318,10 +327,10 @@ def cbind(left,right):
   j = frame(fr)
   fr = j['frames'][0]
   rows = fr['rows']
-  veckeys = fr['vec_keys']
+  vec_ids = fr['vec_ids']
   cols = fr['columns']
   colnames = [col['label'] for col in cols]
-  result = H2OFrame(vecs=H2OVec.new_vecs(zip(colnames, veckeys), rows))
+  result = H2OFrame(vecs=H2OVec.new_vecs(zip(colnames, vec_ids), rows))
   result.setNames(names)
   return result
 